c64dd723eb5022c6dab3baa29ec966ca45905c4d
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-06-21 01:29:24 -0300

    Adicionado course-list

---
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input, input, Output} from '@angular/core';
+import {Course} from "../model/course";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CoursesService} from "../services/courses.service";
+import {async, Observable} from "rxjs";
+import EventEmitter from "node:events";
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrl: './courses-list.component.scss'
+})
+
+export class CoursesListComponent {
+  courses$: Observable<Course[]>;
+
+  //@Input() courses$ : Course[] = [];
+  readonly  displayedColumns = ['name', 'categoria','actions'];
+
+
+  constructor(private router : Router,
+              private route : ActivatedRoute,
+              private coursesService: CoursesService,
+  ) {
+    this.courses$ = this.coursesService.list();
+
+  }
+
+  onAdd(){
+    console.log('onAdd')
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+}
